cd111c7c5c2194992a74738129105199d8fca1fc
BillKek <51540758+BillKek@users.noreply.github.com>
2020-12-24 15:59:22 +0300

    Add files via upload

---
+++ without_extention_FILE_setup_association_notepad_light.bat
@@ -0,0 +1,7 @@
+assoc .="No Extension"
+ftype "No Extension"="notepad" "%1"
+assoc "No Extension"\DefaultIcon=%SystemRoot%\System32\imageres.dll,-%DEFAULT_ICON%
+
+
+REM Light by User // Tested OK on Win7 x64
+REM Orig by, Michael Martin
