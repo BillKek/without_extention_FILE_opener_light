33dd5f3e6443a5411e23d238a8fb12e95c5c82b0
BillKek <51540758+BillKek@users.noreply.github.com>
2021-02-27 02:16:57 +0300

    v2 after testing on fresh win7

--- without_extension_FILE_setup_association_notepad_light.bat
+++ without_extension_FILE_setup_association_notepad_light.bat
@@ -1,7 +1,6 @@
-assoc .="No Extension"
-ftype "No Extension"="notepad" "%1"
-assoc "No Extension"\DefaultIcon=%SystemRoot%\System32\imageres.dll,-%DEFAULT_ICON%
+reg add HKCU\SOFTWARE\Classes\. /v "" /t REG_SZ /d "textdocumentwNoExtension" /f
+reg add HKCU\SOFTWARE\Classes\textdocumentwNoExtension\shell\open\command /v "" /t REG_SZ /d "notepad \"%%1\"" /f
+assoc .="NoExtension"
+ftype ^"NoExtension^"="notepad" "%%1"
+assoc ^"NoExtension^"\DefaultIcon=^%SystemRoot^%\System32\imageres.dll,-102
 
-
-REM Light by User // Tested OK on Win7 x64
-REM Orig by, Michael Martin
